ce1d9e6e48f819c81195358f37767b20ae6baefa
Matt Crider <mattcrider@gmail.com>
2013-03-21 21:33:46 -1000

    Page saving comes correct

--- client/views/edit.js
+++ client/views/edit.js
@@ -2,17 +2,33 @@
 // Describes the page's metadata and actions.
 
 Template.edit.events = {
+  // 'keyup input': function(e) {
+  //   e.preventDefault();
+  //   var page = Template.edit.page();
+  //   var setting = {};
+  //   debugger;
+  //   var attr = $(this).attr('id');
+  //   var val = $(this).val();
+  //   setting[attr] = val;
+
+  //   Pages.update({_id: Pages.findOne({slug: "home"})._id}, {$set: { attr: val }});
+  //   return false;
+  // },
   'submit #pageEditForm': function (e) {
+    page = {};
+    for (var attrname in this) {
+      if(attrname != '_id') page[attrname] = $('#'+attrname).val(); // Clone this without _id (which we can't save)
+    }
     e.preventDefault();
-    var page = Template.edit.page();
-    var pageData = {};
-    $("#pageEditForm input[type='text'], #pageEditForm textarea").each(function() {
-      pageData[$(this).attr('id')] = $(this).val();
-    });
-    Pages.update({_id: Pages.findOne({slug: "home"})._id}, {$set: pageData});
+    Pages.update({_id: this._id}, {$set: page});
     return false;
   }
 };
+
+Template.edit.clone = (function(){
+  return function (obj) { Object.prototype=obj; return new Object() };
+  function Object(){}
+}());
 
 Template.edit.page = function () {
   var page_slug = Session.get('page_slug');
