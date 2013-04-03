77b3cf87a6d4b168e565cd76466f5e2953cebc68
Matt Crider <mattcrider@gmail.com>
2013-04-02 22:46:06 -1000

    Block deletion

--- client/views/blocks/block_zone_editor.js
+++ client/views/blocks/block_zone_editor.js
@@ -1,4 +1,17 @@
 Template.block_zone_editor.added = function() {
   if(this.added) return utils.displayHumanReadableTime(this.added);
   else return '';
-}
+};
+
+Template.block_zone_editor.events = {
+  'click .deleteBlockButton': function() {
+    page = utils.getCurrentPage();
+    // 'this' is the pageBlock, delete from current page
+    Pages.update({ _id : page._id }, {$pull : {  "blocks" : { id: this.id }}});
+    return false;
+  },
+  'click .editBlockButton': function() {
+    alert('bar');
+    return false;
+  }
+};
