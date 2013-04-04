c9b6c9ff37d25599812774d083a8abaeebe19b32
Matt Crider <mattcrider@gmail.com>
2013-04-04 08:03:36 -1000

    Block editing

--- client/views/blocks/block_zone_editor.js
+++ client/views/blocks/block_zone_editor.js
@@ -12,10 +12,33 @@ Template.block_zone_editor.events = {
     $('#delete-block-modal').modal('hide');
     page = utils.getCurrentPage();
     Pages.update({ _id : page._id }, {$pull : {  "blocks" : { id: this.id }}});
+    $.pnotify({
+      text: 'Block removed from page.',
+      type: 'success',
+      icon: false
+    });
     return false;
   },
-  'click .editBlockButton': function() {
-    alert('bar');
+  'click .edit-block-button': function() {
+    var block = Blocks.findOne({_id: this.id});
+    var fragment = Meteor.render(function () {
+      // template = page.template ? page.template : 'page_default';
+      Template[ block.template + "_edit" ].block = block;
+      return Template[ block.template + "_edit" ](block); // this calls the template and returns the HTML.
+    });
+    $('#editBlockModal .modal-body').html(fragment);
+    $('#editBlockModal').modal('show');
+    return false;
+  },
+  'click .edit-block-confirm': function() {
+    $('#editBlockModal').modal('hide');
+    var blockData = utils.getFormValues("#blockEditForm");
+    Blocks.update({_id: this.blocks[0].id}, {$set: blockData});
+    $.pnotify({
+      text: 'Block Saved.',
+      type: 'success',
+      icon: false
+    });
     return false;
   }
 };
