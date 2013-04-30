6f69ab9f654e14e6259febc9539dbe4f1f3e0139
Matt Crider <mattcrider@gmail.com>
2013-04-29 19:02:40 -1000

    Attempt at fixing wysiwyg editor in modals

--- client/views/form/wysiwyg.js
+++ client/views/form/wysiwyg.js
@@ -1,19 +1,31 @@
 Template.wysiwyg.rendered = function() {
-  $('a[title]').tooltip({container:'body'});
-  $('.wysiwyg-editor').wysiwyg();
-  $('.dropdown-menu input').click(function() {return false;})
+  debugger;
+  $(this.findAll('a[title]')).tooltip({container:'body'});
+  
+  // Hack to get multiple instances of editor to work
+  if (this.data.uniqueId) {
+    $('#' + this.data.uniqueId + '_wysiwyg').wysiwyg({toolbarSelector: '#' + this.data.uniqueId + '_toolbar'});
+  } else {
+    $('.wysiwyg-editor').wysiwyg();
+  }
+
+  
+  $(this.findAll('.dropdown-menu input')).click(function() {return false;})
       .change(function () {$(this).parent('.dropdown-menu').siblings('.dropdown-toggle').dropdown('toggle');})
       .keydown('esc', function () {this.value='';$(this).change();});
 }
 
 Template.wysiwyg.events = {
   'keyup .wysiwyg-editor': function(e) {
+    debugger;
     $(e.currentTarget).next('.html-editor').val($(e.currentTarget).cleanHtml());
   },
   'keyup .html-editor': function(e) {
+    debugger;
     $(e.currentTarget).prev('.wysiwyg-editor').html($(e.currentTarget).val());
   },
   'click .html-editor-button': function(e) {
+    debugger;
     $(e.currentTarget).toggleClass('btn-info');
     $(e.currentTarget).closest('.btn-toolbar').next('.wysiwyg-editor').toggleClass('hide');
     $(e.currentTarget).closest('.btn-toolbar').next().next('.html-editor').toggleClass('hide');
