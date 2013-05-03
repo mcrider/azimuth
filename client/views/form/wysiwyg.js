ed102cf2ea71da82ccb6ec3b49addeee5783196e
Matt Crider <mattcrider@gmail.com>
2013-05-02 22:45:33 -1000

    Got wysiwyg to work in blocks, albeit hackily

--- client/views/form/wysiwyg.js
+++ client/views/form/wysiwyg.js
@@ -1,15 +1,64 @@
 Template.wysiwyg.rendered = function() {
-  debugger;
+
   $(this.findAll('a[title]')).tooltip({container:'body'});
+
+  // FIXME: Hack: Need to specify events here as the template events don't attach to editors in blocks
+
+  var onWysiwygKeyup = function(e) {
+    $(e.currentTarget).next('.html-editor').val($(e.currentTarget).cleanHtml());
+  }
+
+  var onHtmlKeyup = function(e) {
+    $(e.currentTarget).prev('.wysiwyg-editor').html($(e.currentTarget).val());
+  }
+
+  onHtmlButtonClick = function(e) {
+    $(e.currentTarget).toggleClass('btn-info');
+    $(e.currentTarget).closest('.btn-toolbar').next('.wysiwyg-editor').toggleClass('hide');
+    $(e.currentTarget).closest('.btn-toolbar').next().next('.html-editor').toggleClass('hide');
+  }
+
+  onImageClick = function(e) {
+    filepicker.setKey(Settings.findOne().filepickerKey);
+    var $inputArea = $(e.currentTarget).closest('.btn-toolbar').next('.wysiwyg-editor');
+    filepicker.pick({
+        mimetypes: ['image/*']
+      }, 
+      function(FPFile){
+        $inputArea.focus();
+        document.execCommand('insertimage', 0, FPFile.url);
+        console.log(FPFile.url);
+      }
+    );
+  }
+
+  onFileClick = function(e) {
+    filepicker.setKey(Settings.findOne().filepickerKey);
+    var $inputArea = $(e.currentTarget).closest('.btn-toolbar').next('.wysiwyg-editor');
+    filepicker.pick(
+      function(FPFile){
+        $inputArea.focus();
+        document.execCommand('createLink', 0, FPFile.url);
+        console.log(FPFile.url);
+      }
+    );
+  }
   
   // Hack to get multiple instances of editor to work
   if (this.data.uniqueId) {
     $('#' + this.data.uniqueId + '_wysiwyg').wysiwyg({toolbarSelector: '#' + this.data.uniqueId + '_toolbar'});
+    $('#' + this.data.uniqueId + '_wysiwyg').on('keyup', onWysiwygKeyup);
+    $('#' + this.data.uniqueId + '_html').on('keyup', onHtmlKeyup);
+
+
+    $('#' + this.data.uniqueId + '_toolbar .html-editor-button').on('click', onHtmlButtonClick);
+    $('#' + this.data.uniqueId + '_toolbar .filepicker-image').on('click', onImageClick);
+    $('#' + this.data.uniqueId + '_toolbar .filepicker-file').on('click', onFileClick);
+
   } else {
     $('.wysiwyg-editor').wysiwyg();
   }
 
-  
   $(this.findAll('.dropdown-menu input')).click(function() {return false;})
       .change(function () {$(this).parent('.dropdown-menu').siblings('.dropdown-toggle').dropdown('toggle');})
       .keydown('esc', function () {this.value='';$(this).change();});
@@ -17,15 +66,12 @@ Template.wysiwyg.rendered = function() {
 
 Template.wysiwyg.events = {
   'keyup .wysiwyg-editor': function(e) {
-    debugger;
     $(e.currentTarget).next('.html-editor').val($(e.currentTarget).cleanHtml());
   },
   'keyup .html-editor': function(e) {
-    debugger;
     $(e.currentTarget).prev('.wysiwyg-editor').html($(e.currentTarget).val());
   },
   'click .html-editor-button': function(e) {
-    debugger;
     $(e.currentTarget).toggleClass('btn-info');
     $(e.currentTarget).closest('.btn-toolbar').next('.wysiwyg-editor').toggleClass('hide');
     $(e.currentTarget).closest('.btn-toolbar').next().next('.html-editor').toggleClass('hide');
