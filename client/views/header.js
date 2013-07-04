5938c6c91561d97a35c011efe1da70dd789ad113
Matt Crider <mattcrider@gmail.com>
2013-07-04 12:58:53 -1000

    Resolved issues with login/account creation.  Fixes #23

--- client/views/header.js
+++ client/views/header.js
@@ -4,17 +4,28 @@
 Template.header.rendered = function() {
   // Set page title
   document.title = utils.getSetting('siteName');
-
   // Hack to make login menu an icon rather than username and some other tweaks to accounts-ui-bootstrap-dropdown package
   $('#login-buttons .dropdown').removeClass('dropdown');
   var username = $('#login-dropdown-list .dropdown-toggle').first().text();
   $('#login-dropdown-list .dropdown-menu').prepend('<div class="nav-header">'+username+'</div>');
   $('#login-dropdown-list .dropdown-toggle').html('<i class="icon-user'+ (Meteor.user() ? ' logged-in':'')+'"></i> <b class="caret"></b>');
 
-  // Fade in the page
-  $(".loading-overlay").fadeOut('slow', function() {
-   $("#contents").hide().removeClass('hidden').fadeIn('slow');
-  });
+  // Remove mobile/desktop loginButtons (having two {loginButtons} loaded causes errors with accounts-ui-bootstrap-dropdown)
+  if($('.mobile-login').is(":visible")) {
+    $('.desktop-login').remove();
+  } else {
+    $('.mobile-login').remove();
+  }
+
+  // FIXME: Find out why this template is being re-rendered e.g. when clicking login buttons ({{#isolate}} does not help)
+  if(!this.headerRendered) {
+    // Fade in the page
+    $(".loading-overlay").fadeOut('slow', function() {
+     $("#contents").hide().removeClass('hidden').fadeIn('slow');
+    });
+
+    this.headerRendered = true;
+  }
 }
 
 Template.header.helpers({
