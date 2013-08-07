59473dc275d620041a9e88f7f510bf1913229798
Kjetil Geirbo <kjetil@geirbo.no>
2013-08-07 20:35:06 +0200

    fixed: Add Existing Block -> insert

--- client/views/blocks/modals/existing_block.js
+++ client/views/blocks/modals/existing_block.js
@@ -15,6 +15,8 @@ Template.existing_block.events = {
     var page = utils.getCurrentPage();
 
     var label = Template[this.template].label || 'Single Block';
+    //find the block by the (block)-id of the insert button
+    block = Blocks.findOne(e.target.id);
     // Attach the block to the page
     if (!page.notFound) {
       PageBlocks.insert({page_id: page._id, block_id: block._id, label: label, zone: Session.get('block-zone'), added: Date.now()});
